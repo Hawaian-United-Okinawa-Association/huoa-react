54550b93a7fadb6a657d1139dadad3fcdf34b9e6
Michael <mcheng.oc@gmail.com>
2020-06-18 20:36:13 -0700

    update(nav): updates text

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Testing Github FTP Deploy</h1>
+				<h1>Testing Github FTP Deploys</h1>
 				<Link to="/">
 					<h4>HUOA Home</h4>
 				</Link>
