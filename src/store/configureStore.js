a307ffc2f559ac2ea7f77a1da3d9d1c33cb5d732
Marco <58441048+masalcedo88@users.noreply.github.com>
2020-06-23 16:40:00 -0700

    Update configureStore.js

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,2 +1,3 @@
-module.exports =
-	process.env.NODE_ENV === 'production' ? require('./configureStore.prod') : require('./configureStore.dev');
+module.exports = process.env.NODE_ENV === 'production' ?
+	require('./configureStore.prod') :
+	require('./configureStore.dev');
