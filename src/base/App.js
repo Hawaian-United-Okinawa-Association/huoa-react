9198ea12c2a136e9ea2f6b8443a865a86160c24c
Michael <mcheng.oc@gmail.com>
2020-06-26 12:33:54 -0700

    updates(App): adds Modal Manager to App

--- src/base/App.js
+++ src/base/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 
 //Components
+import ModalManager from './../components/Modal/ModalManager'
 import Navbar from './../components/Navbar/Navbar';
 import Footer from './../components/Footer/Footer';
 import Routes from './../config/routes';
@@ -9,6 +10,7 @@ import Routes from './../config/routes';
 const App = () => {
 	return (
 		<main>
+			<ModalManager />
 			<Navbar />
 			<Routes />
 			<Footer />
