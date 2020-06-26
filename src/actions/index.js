795b013a6aeb65e340f036f38b051b431894247e
Michael <mcheng.oc@gmail.com>
2020-06-26 12:31:47 -0700

    feat(Modal): adds actions for openModal and closeModal

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import content from '../apis/content';
-import { CREATE_EVENT, FETCH_EVENT, FETCH_EVENTS, DELETE_EVENT, EDIT_EVENT } from './actionTypes';
+import { CREATE_EVENT, FETCH_EVENT, FETCH_EVENTS, DELETE_EVENT, EDIT_EVENT, MODAL_CLOSE, MODAL_OPEN } from './actionTypes';
 
 //Event Actions Examples
 export const fetchEvents = () => async (dispatch) => {
@@ -13,3 +13,19 @@ export const deleteEvent = (id) => async (dispatch) => {
 
 	dispatch({ type: DELETE_EVENT, payload: id });
 };
+
+export const openModal = (modalType, modalProps) => {
+	console.log('hits openModal action')
+	console.log(modalType);
+	console.log(modalProps);
+	return {
+		type: MODAL_OPEN,
+		payload: { modalType, modalProps }
+	};
+}
+
+export const closeModal = () => {
+	return {
+		type: MODAL_CLOSE
+	};
+}
