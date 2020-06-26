d53b4c6051bf15a1a15ef85f6c19ab78f93d85a0
Michael <mcheng.oc@gmail.com>
2020-06-26 12:34:19 -0700

    feat(Footer): adds Modal button to Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,21 @@
 //Dependencies
 import React from 'react';
+import { connect } from 'react-redux';
+import { openModal } from './../../actions/index';
 
-const Footer = () => {
+const actions = { openModal };
+
+const Footer = (props) => {
+	const onOpenModalClicked = () => {
+		openModal("TestModal", { a : 42});
+	}
+
 	return (
 		<div>
 			<h4>Footer</h4>
+			<button onClick={() => onOpenModalClicked()}>Click for modal.</button>
 		</div>
 	);
 };
 
-export default Footer;
+export default connect(null, actions)(Footer);
