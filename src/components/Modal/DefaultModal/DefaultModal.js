f2990370d928f691329337f395498f195cca255c
Michael <mcheng.oc@gmail.com>
2020-06-30 15:30:56 -0700

    styles(DefaultModal): styles modal to match prototype

--- src/components/Modal/DefaultModal/DefaultModal.js
+++ src/components/Modal/DefaultModal/DefaultModal.js
@@ -15,12 +15,18 @@ const DefaultModal = (props) => {
   return (
     <div className='modal'>
       <div className='modal--overlay'></div>
-      <div className='modal--content'>
-        <h1>Default Modal</h1>
-        <button onClick={() => closeModal()}>Close Modal</button>
+      <div className='modal--container'>
+        <div className='modal--close' onClick={() => closeModal()}>X</div>
+        <div className='modal--content'>
+          <h1>You are one of us now!</h1>
+          <p>Thank you for becoming a part of our community.</p>
+          <p>Keep an eye on your inbox, we will send you an email at xztuhda@gmail.com.</p>
+        </div>
       </div>
     </div>
   )
 }
 
 export default DefaultModal;
+
+// onClick={() => closeModal()}
