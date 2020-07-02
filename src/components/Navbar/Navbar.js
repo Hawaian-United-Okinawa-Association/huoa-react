21c947c9ac04e5cbc5cd3c87a71ea97f5c67e05e
Marco Salcedo <masalcedo.88@gmail.com>
2020-07-01 20:22:27 -0700

    creates color variables @ styles/colors.scss && creates assets directory with HUOA logo files && creates Navbar.scss for navbar styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,14 +2,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <div>
-        <Link to="/">
-          <h4>HUOA Home</h4>
-        </Link>
-        <ul>
+//Components, assets, actions, etc..
+import logo from '../../assets/huoa-logo.png';
+
+//styles
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-brand">
+        <div className="nav-img">
+          <Link to="/">
+            <img className="nav-logo" src={logo} />
+          </Link>
+        </div>
+        <div className="nav-text">
+          <h4>Hawaii United Okinawa Association</h4>
+          <p>Celebrating 120 years of Uchinanchu in Hawaii</p>
+        </div>
+      </div>
+      <div className="nav-block">
+        <ul className="nav-links">
           <Link to="/about">
             <li>About</li>
           </Link>
@@ -28,13 +42,13 @@ class Navbar extends React.Component {
           <Link to="/contact">
             <li>Contact</li>
           </Link>
-          <Link to="/donate">
-            <li>Donate</li>
+          <Link className="donate-btn" to="/donate">
+            <li className="donate-btn-txt">Donate</li>
           </Link>
         </ul>
       </div>
-    );
-  }
-}
+    </nav>
+  );
+};
 
 export default Navbar;
