47a6e3730ebae2db1538af632cf4ac76436d38fa
jason-toups <me@jasontoups.com>
2020-07-23 15:22:00 -0700

    adds vscode to gitignore & initial container components & styling

---
+++ src/components/Container/Contents.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import './Container.scss';
+
+class Contents extends React.Component {
+	render() {
+		return (
+			<div className='container'>
+				<div className='grid=item'>
+					<h1>1</h1>
+				</div>
+				<div className='grid=item'>
+					<h1>2</h1>
+				</div>
+				<div className='grid=item'>
+					<h1>3</h1>
+				</div>
+				<div className='grid=item'>
+					<h1>4</h1>
+				</div>
+				<div className='grid=item'>
+					<h1>5</h1>
+				</div>
+				<div className='grid=item'>
+					<h1>6</h1>
+				</div>
+				<div className='grid=item'>
+					<h1>7</h1>
+				</div>
+				<div className='grid=item'>
+					<h1>8</h1>
+				</div>
+				<div className='grid=item'>
+					<h1>9</h1>
+				</div>
+				<div className='grid=item'>
+					<h1>10</h1>
+				</div>
+				<div className='grid=item'>
+					<h1>11</h1>
+				</div>
+				<div className='grid=item'>
+					<h1>12</h1>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Contents;
