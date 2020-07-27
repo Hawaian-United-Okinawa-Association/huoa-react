488f46559d7087410f6af3e62161a14fb83b0e99
Michael <mcheng.oc@gmail.com>
2020-07-27 14:51:31 -0700

    feat(App): adds axios call to api to get acf data

--- src/base/App.js
+++ src/base/App.js
@@ -1,6 +1,6 @@
 //Dependencies
 import React from 'react';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 
 //Components
 import Modal from './../components/Modal/Modal';
@@ -8,11 +8,14 @@ import Navbar from './../components/Navbar/Navbar';
 import Footer from './../components/Footer/Footer';
 import Routes from './../config/routes';
 
-import { fetchEvents } from './../actions/index';
+import { fetchEvents, getPages } from './../actions/index';
 
 import './App.scss';
 
 const App = (props) => {
+  const dispatch = useDispatch();
+  dispatch(getPages());
+
   return (
     <main>
       <Modal />
