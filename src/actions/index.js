207a63c7b5e72a3388c87bd3db331f00b4c412f3
Michael <mcheng.oc@gmail.com>
2020-07-28 15:58:25 -0700

    chore(actions): fixes syntax and renames api const

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,12 @@
-import { MODAL_OPEN, MODAL_CLOSE, GET_PAGES } from './actionTypes';
 import axios from 'axios';
 
-const apiRoot = 'http://dev.huoa.org/wp-json';
+import { GET_PAGES, MODAL_OPEN, MODAL_CLOSE } from './actionTypes';
+
+const api = 'http://dev.huoa.org/wp-json';
 
 export const getPages = () => async (dispatch) => {
-  const response = await axios.get(`${apiRoot}/wp/v2/pages?page=1&per_page=100`);
-  const data = response.data.reduce((allData, data) => ({ ...allData, [data.slug]: { ...data.acf} }), {})
+  const response = await axios.get(`${api}/wp/v2/pages?page=1&per_page=100`);
+  const data = response.data.reduce((allData, data) => ({ ...allData, [data.slug]: { ...data.acf} }), {});
   dispatch({ type: GET_PAGES, payload: data });
 };
 
