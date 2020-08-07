7997d2baf5459caff9b3c46d82b5c83e37317386
jason-toups <me@jasontoups.com>
2020-08-06 18:41:19 -0700

    create (container component)

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,23 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './Container.scss';
 
-// Can we pass a property that dictates how many columns a div takes up?
-
-// React Bootstrap shows examples of determining if the children is a col, or a row. In {props.children} could find out if it's a col or row.
-
-// I could make this declarative within the JSX, instead of relying on styling it in CSS.
-
-// TODO current problem, the container is handling grid, but it passes the children into the first grid of the container.
-
-// TODO the amount of columns should be declarative, when writing
-
 const Container = props => {
-  return (
-    <div className={'container' + (props.layout ? ' ' + props.layout : '')}>
-      {props.children}
-    </div>
-  );
+  if (props.layout) {
+    return <div className={'container ' + props.layout}>{props.children}</div>;
+  }
+  return <div className={'container'}>{props.children}</div>;
 };
 
 export default Container;
