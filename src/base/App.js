b1b4c7851278ba673538e3977c0d1736e914a836
jason-toups <me@jasontoups.com>
2020-08-06 18:48:54 -0700

    fix (App changes were committed during dev, reverting back to main)

--- src/base/App.js
+++ src/base/App.js
@@ -1,44 +1,29 @@
 //Dependencies
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 //Components
 import Modal from './../components/Modal/Modal';
 import Navbar from './../components/Navbar/Navbar';
 import Footer from './../components/Footer/Footer';
-import Container from './../components/Container/Container';
-// import Column from './../components/Container/Column/Column';
-// import Row from './../components/Container/Row/Row';
-// import Contents from '../components/Container/Contents3Items';
 import Routes from './../config/routes';
 
-import { fetchEvents } from './../actions/index';
+import { getPages } from './../actions/index';
 
 import './App.scss';
-import Contents3Items from 'components/Container/Contents3Items';
-import Contents4Items from 'components/Container/Contents4Items';
-import Contents4ItemsImage from 'components/Container/Contents4ItemsImage';
-
-//TODO remove container and contents before making a pull request into main.
 
 const App = props => {
+  const dispatch = useDispatch();
+  dispatch(getPages());
+
   return (
     <main>
       <Modal />
       <Navbar />
       <Routes />
       <Footer />
-      <Container layout='cols-3'>
-        <Contents3Items></Contents3Items>
-      </Container>
-      <Container layout='cols-4'>
-        <Contents4Items></Contents4Items>
-      </Container>
-      <Container layout='cols-4-image'>
-        <Contents4ItemsImage></Contents4ItemsImage>
-      </Container>
     </main>
   );
 };
 
-export default connect(null, { fetchEvents })(App);
+export default App;
