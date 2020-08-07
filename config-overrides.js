7990dacf38aeb951f30146fdacc25331f375bf41
Michael <mcheng.oc@gmail.com>
2020-08-07 11:40:27 -0700

    update(style-lint): fixes kebab case validation and switches element and modifier in Modal and Footer

--- config-overrides.js
+++ config-overrides.js
@@ -2,20 +2,17 @@
 const StylelintPlugin = require('stylelint-webpack-plugin')
 
 module.exports = {
-  webpack: function (config, env) {
-    if (env === 'development') {
-      config.plugins.push(
-        new StylelintPlugin({
-          configFile: 'stylelint.config.js',
-          context: 'src',
-          files: '**/*.scss',
-          failOnError: false,
-          quiet: false,
-          emitErrors: true
-        })
-      )
-    }
-
+  webpack: function (config) {
+    config.plugins.push(
+      new StylelintPlugin({
+        configFile: 'stylelint.config.js',
+        context: 'src',
+        files: '**/*.scss',
+        failOnError: false,
+        quiet: false,
+        emitErrors: true
+      })
+    )
     return config
   }
 }
