d8745277e3bd8c8124b19c37c2d001c0533e89d6
jason-toups <me@jasontoups.com>
2020-08-10 16:19:07 -0700

    fix(index action) reverts file back to main

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,26 +2,23 @@ import axios from 'axios';
 
 import { GET_PAGES, MODAL_OPEN, MODAL_CLOSE } from './actionTypes';
 
-const api = 'https://dev.huoa.org/wp-json';
+const api = 'http://dev.huoa.org/wp-json';
 
-export const getPages = () => async dispatch => {
+export const getPages = () => async (dispatch) => {
   const response = await axios.get(`${api}/wp/v2/pages?page=1&per_page=100`);
-  const data = response.data.reduce(
-    (allData, data) => ({ ...allData, [data.slug]: { ...data.acf } }),
-    {},
-  );
+  const data = response.data.reduce((allData, data) => ({ ...allData, [data.slug]: { ...data.acf} }), {});
   dispatch({ type: GET_PAGES, payload: data });
 };
 
-export const openModal = modalContent => {
+export const openModal = (modalContent) => {
   return {
     type: MODAL_OPEN,
-    payload: modalContent,
+    payload: modalContent
   };
 };
 
 export const closeModal = () => {
   return {
-    type: MODAL_CLOSE,
+    type: MODAL_CLOSE
   };
 };
