0b672a18964e6070fd809e1d729d0b659c3df2c3
jason-toups <me@jasontoups.com>
2020-08-18 20:38:35 -0700

    create

---
+++ src/components/Cards/CardSmall/CardSmall.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './CardSmall.scss';
+
+const CardSmall = ({ image, imageAlt, header, paragraph, button }) => {
+  return (
+    <div className='card-small'>
+      <img className='card-small__image' src={`${image}`} alt={`${imageAlt}`}/>
+      <div className='card-small__text'>
+        <h3 className='card-small__header'>{`${header}`}</h3>
+        <p className='card-small__paragraph'>{`${paragraph}`}</p>
+      </div>
+      <button className='card-small__button'>{`${button}`}</button>
+    </div>
+  );
+};
+
+export default CardSmall;
