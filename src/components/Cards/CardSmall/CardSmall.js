2615449182beb23eb94abf0ecb105e3057842a69
jason-toups <me@jasontoups.com>
2020-08-19 10:42:58 -0700

    fix(card): removes template literals

--- src/components/Cards/CardSmall/CardSmall.js
+++ src/components/Cards/CardSmall/CardSmall.js
@@ -5,12 +5,12 @@ import './CardSmall.scss';
 const CardSmall = ({ image, imageAlt, header, paragraph, button }) => {
   return (
     <div className='card-small'>
-      <img className='card-small__image' src={`${image}`} alt={`${imageAlt}`}/>
+      <img className='card-small__image' src={image} alt={`${imageAlt}`}/>
       <div className='card-small__text'>
-        <h3 className='card-small__header'>{`${header}`}</h3>
-        <p className='card-small__paragraph'>{`${paragraph}`}</p>
+        <h3 className='card-small__header'>{header}</h3>
+        <p className='card-small__paragraph'>{paragraph}</p>
       </div>
-      <button className='card-small__button'>{`${button}`}</button>
+      <button className='card-small__button'>{button}</button>
     </div>
   );
 };
--- src/components/Cards/CardSquare/CardSquare.js
+++ src/components/Cards/CardSquare/CardSquare.js
@@ -6,11 +6,11 @@ const CardSquare = ({ header, subheader, paragraph, button }) => {
   return (
     <div className='card-square'>
       <div className='card-square__text'>
-        <h1 className='card-square__header'>{`${header}`}</h1>
-        <h3 className='card-square__subheader'>{`${subheader}`}</h3>
-        <p className='card-square__paragraph'>{`${paragraph}`}</p>
+        <h1 className='card-square__header'>{header}</h1>
+        <h3 className='card-square__subheader'>{subheader}</h3>
+        <p className='card-square__paragraph'>{paragraph}</p>
       </div>
-      <button className='card-square__button'>{`${button}`}</button>
+      <button className='card-square__button'>{button}</button>
     </div>
   );
 };
