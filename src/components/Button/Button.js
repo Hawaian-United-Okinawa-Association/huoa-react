98eedc54695edb8322b108789b4018d2b42b9e70
Michael <mcheng.oc@gmail.com>
2020-08-18 17:34:11 -0700

    update(Button): adds plus svg, removes dispatch, adds type text button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { openModal } from './../../actions/index';
 
+import { ReactComponent as PlusIcon } from 'assets/plus-icon.svg';
 import './Button.scss';
 
-const Button = ({ type, icon, children, link, modal, click, disabled }) => {
-  let plusIcon = icon ? <span>+ </span> : null;
-  let dispatch = useDispatch();
+const Button = ({ type, icon, children, link, onClick, disabled }) => {
+  let plusIcon = icon ? <PlusIcon className='button__plus-icon'/> : null;
 
   if (link) {
     if (link.charAt(0) === '/') return <Link to={link} className='button' type={type}>{plusIcon}{children}</Link>;
-    else return <a className='button' href={link} type={type}>{plusIcon}{children}</a>;
+    else return <a href={link} className='button' type={type}>{plusIcon}{children}</a>;
   }
-  else if (modal) return <button className='button' type={type} onClick={() => dispatch(openModal(modal))}>{plusIcon}{children}</button>;
-  else if (click) return <button className='button' type={type} onClick={() => click()} disabled={disabled}>{plusIcon}{children}</button>;
-  else throw new Error('Button requires link, modal or click attribute');
+  else if (onClick) return <button className='button' type={type} onClick={onClick} disabled={disabled}>{plusIcon}{children}</button>;
+  else throw new Error('Button requires a link or an onClick attribute');
 }
 
 export default Button;
--- src/pages/Donate/Donate.js
+++ src/pages/Donate/Donate.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { openModal } from 'actions/index';
 
-import Button from './../../components/Button/Button';
-import CustomThankYou from './../../components/Custom/CustomThankYou/CustomThankYou';
+import Button from 'components/Button/Button';
+import CustomThankYou from 'components/Custom/CustomThankYou/CustomThankYou';
 
 const Donate = () => {
+  const dispatch = useDispatch();
   const thankYou = <CustomThankYou />
 
   return (
     <div>
       <h4>Donate</h4>
-      <Button type='filled' modal={thankYou}>Opens Modal</Button>
+      <Button type='filled' onClick={() => dispatch(openModal(thankYou))} icon>Opens Modal</Button>
+      <Button type='outlined' onClick={() => console.log('hello')} icon>Say Hello</Button>
+      <Button type='text' onClick={() => console.log('hello')} icon>Button Text</Button>
     </div>
   )
 }
