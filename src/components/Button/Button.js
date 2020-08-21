614b2edbd1adb2206f23c153fdd3f35ec6c38df8
Michael <mcheng.oc@gmail.com>
2020-08-20 18:28:28 -0700

    update(Button): adds ref, adds regex logic for link, hoists error up button if/else

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,15 +4,25 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as PlusIcon } from 'assets/plus-icon.svg';
 import './Button.scss';
 
-const Button = ({ type, icon, children, link, onClick, disabled }) => {
-  let plusIcon = icon ? <PlusIcon className='button__plus-icon'/> : null;
-
-  if (link) {
-    if (link.charAt(0) === '/') return <Link to={link} className='button' type={type}>{plusIcon}{children}</Link>;
-    else return <a href={link} className='button' type={type}>{plusIcon}{children}</a>;
+const Button = ({ type, icon, buttonRef, children, link, onClick, disabled }) => {
+  const rootURL = new RegExp('huoa.org/')
+  if (rootURL.test(link)) {
+    let match = rootURL.exec(link);
+    link = link.slice(match.index + 8)
   }
-  else if (onClick) return <button className='button' type={type} onClick={onClick} disabled={disabled}>{plusIcon}{children}</button>;
-  else throw new Error('Button requires a link or an onClick attribute');
+
+  if (!onClick && !link) {
+    throw new Error('Button requires a link or an onClick attribute');
+  } 
+  else if (onClick) {
+    return <button className='button' type={type} ref={buttonRef} onClick={onClick} disabled={disabled}>{icon ? <PlusIcon className='button__plus-icon'/> : null }{children}</button>;
+  } 
+  else if (link.charAt(0) === '/') {
+    return <Link to={link} className='button' type={type}>{icon ? <PlusIcon className='button__plus-icon'/> : null }{children}</Link>;
+  } 
+  else {
+    return <a href={link} className='button' type={type}>{icon ? <PlusIcon className='button__plus-icon'/> : null }{children}</a>;
+  }
 }
 
 export default Button;
--- src/pages/Donate/Donate.js
+++ src/pages/Donate/Donate.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { openModal } from 'actions/index';
 
@@ -8,13 +8,16 @@ import CustomThankYou from 'components/Custom/CustomThankYou/CustomThankYou';
 const Donate = () => {
   const dispatch = useDispatch();
   const thankYou = <CustomThankYou />
+  const buttonRef = useRef(null);
 
   return (
     <div>
       <h4>Donate</h4>
-      <Button type='filled' onClick={() => dispatch(openModal(thankYou))} icon>Opens Modal</Button>
-      <Button type='outlined' onClick={() => console.log('hello')} icon>Say Hello</Button>
-      <Button type='text' onClick={() => console.log('hello')} icon>Button Text</Button>
+      <Button type='filled' buttonRef={buttonRef} onClick={() => dispatch(openModal(thankYou))} icon>Opens Modal</Button>
+      <Button type='outlined' buttonRef={buttonRef} onClick={() => console.log('hello')} icon>Say Hello</Button>
+      <Button type='text' buttonRef={buttonRef} onClick={() => console.log('hello')} icon>Button Text</Button>
+      <Button link='https://huoa.org/contact' buttonRef={buttonRef}>Homepage</Button>
+      <Button link='https://www.google.com/' buttonRef={buttonRef}>Google</Button>
     </div>
   )
 }
