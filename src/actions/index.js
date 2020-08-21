33243b69978f66e12d40c8302f18bbe6e86f1333
Matt Laguardia <matthewlaguardia@gmail.com>
2020-08-21 16:56:17 -0700

    updates(actions): adds spacing for readability

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,7 @@ const api = 'https://dev.huoa.org/wp-json';
 export const getPages = () => async (dispatch) => {
   const response = await axios.get(`${api}/wp/v2/pages?page=1&per_page=100`);
   const data = response.data.reduce((allData, data) => ({ ...allData, [data.slug]: { ...data.acf} }), {});
+
   dispatch({ type: GET_PAGES, payload: data });
 };
 
