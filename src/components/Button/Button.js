097dfb3ac3d08bb3be0d0cb6af066c5ad7df6e14
Michael <mcheng.oc@gmail.com>
2020-08-25 11:25:21 -0700

    update(button): wraps Button in forwardRef

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,25 +4,42 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as PlusIcon } from 'assets/plus-icon.svg';
 import './Button.scss';
 
-const Button = ({ type, icon, buttonRef, children, link, onClick, disabled }) => {
+const Button = React.forwardRef((props, ref) => {
+  let { type, icon, children, link, onClick, disabled } = props;
+
   const rootURL = new RegExp('huoa.org/')
   if (rootURL.test(link)) {
     let match = rootURL.exec(link);
     link = link.slice(match.index + 8)
   }
-
+  
   if (!onClick && !link) {
     throw new Error('Button requires a link or an onClick attribute');
   } 
   else if (onClick) {
-    return <button className='button' type={type} ref={buttonRef} onClick={onClick} disabled={disabled}>{icon ? <PlusIcon className='button__plus-icon'/> : null }{children}</button>;
+    return (
+      <button className='button' type={type} ref={ref} onClick={onClick} disabled={disabled}>
+        {icon ? <PlusIcon className='button__plus-icon'/> : null }
+        {children}
+      </button>
+    );
   } 
   else if (link.charAt(0) === '/') {
-    return <Link to={link} className='button' type={type}>{icon ? <PlusIcon className='button__plus-icon'/> : null }{children}</Link>;
+    return (
+      <Link to={link} className='button' type={type}>
+        {icon ? <PlusIcon className='button__plus-icon'/> : null }
+        {children}
+      </Link>
+    )
   } 
   else {
-    return <a href={link} className='button' type={type}>{icon ? <PlusIcon className='button__plus-icon'/> : null }{children}</a>;
+    return (
+      <a href={link} className='button' type={type}>
+        {icon ? <PlusIcon className='button__plus-icon'/> : null }
+        {children}
+      </a>
+    )
   }
-}
+});
 
 export default Button;
--- src/pages/Donate/Donate.js
+++ src/pages/Donate/Donate.js
@@ -8,16 +8,16 @@ import CustomThankYou from 'components/Custom/CustomThankYou/CustomThankYou';
 const Donate = () => {
   const dispatch = useDispatch();
   const thankYou = <CustomThankYou />
-  const buttonRef = useRef(null);
+  const ref = useRef();
+  const refTwo = useRef();
 
   return (
     <div>
       <h4>Donate</h4>
-      <Button type='filled' buttonRef={buttonRef} onClick={() => dispatch(openModal(thankYou))} icon>Opens Modal</Button>
-      <Button type='outlined' buttonRef={buttonRef} onClick={() => console.log('hello')} icon>Say Hello</Button>
-      <Button type='text' buttonRef={buttonRef} onClick={() => console.log('hello')} icon>Button Text</Button>
-      <Button link='https://huoa.org/contact' buttonRef={buttonRef}>Homepage</Button>
-      <Button link='https://www.google.com/' buttonRef={buttonRef}>Google</Button>
+      <Button type='filled' ref={ref} onClick={() => dispatch(openModal(thankYou))} icon>Opens Modal</Button>
+      <Button type='outlined' ref={refTwo} onClick={() => console.log('hello')} icon>Say Hello</Button>
+      <Button type='text' onClick={() => console.log(ref.current.innerHTML = 'Changed Text')} icon>Change Text on Modal Btn</Button>
+      <Button type='filled' onClick={() => console.log(refTwo.current.innerHTML = 'Changed Text')} icon>Change Text on Hello Btn</Button>
     </div>
   )
 }
