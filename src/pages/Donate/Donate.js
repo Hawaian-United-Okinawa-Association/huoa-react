12cccc512566f455913c89d5631a483b29a02850
Michael <mcheng.oc@gmail.com>
2020-08-25 12:28:28 -0700

    create(demo): creates demo page and puts Container and Button in there

--- src/pages/Donate/Donate.js
+++ src/pages/Donate/Donate.js
@@ -1,23 +1,9 @@
-import React, { useRef } from 'react';
-import { useDispatch } from 'react-redux';
-import { openModal } from 'actions/index';
-
-import Button from 'components/Button/Button';
-import CustomThankYou from 'components/Custom/CustomThankYou/CustomThankYou';
+import React from 'react';
 
 const Donate = () => {
-  const dispatch = useDispatch();
-  const thankYou = <CustomThankYou />
-  const ref = useRef();
-  const refTwo = useRef();
-
   return (
     <div>
       <h4>Donate</h4>
-      <Button type='filled' ref={ref} onClick={() => dispatch(openModal(thankYou))} icon>Opens Modal</Button>
-      <Button type='outlined' ref={refTwo} onClick={() => console.log('hello')} icon>Say Hello</Button>
-      <Button type='text' onClick={() => console.log(ref.current.innerHTML = 'Changed Text')} icon>Change Text on Modal Btn</Button>
-      <Button type='filled' onClick={() => console.log(refTwo.current.innerHTML = 'Changed Text')} icon>Change Text on Hello Btn</Button>
     </div>
   )
 }
