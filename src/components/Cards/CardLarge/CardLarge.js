09ec1ca6abc9ad175a286ede30fe0bcf2e40849c
jason-toups <me@jasontoups.com>
2020-08-28 10:56:09 -0700

    feat(card): changes h1 to p for header

--- src/components/Cards/CardLarge/CardLarge.js
+++ src/components/Cards/CardLarge/CardLarge.js
@@ -7,7 +7,7 @@ const CardLarge = ({ image, imageAlt, header, paragraph, button }) => {
     <div className='card-large'>
       <img className='card-large__image' src={image} alt={imageAlt}/>
       <div className='card-large__text'>
-        <h1 className='card-large__header'>{header}</h1>
+        <p className='card-large__header'>{header}</p>
         <p className='card-large__paragraph'>{paragraph}</p>
       </div>
       <button className='card-large__button'>{button}</button>
