06bf64f5c482288ac11fc4059bcbc8d7048d0708
jason-toups <me@jasontoups.com>
2020-09-15 13:21:04 -0700

    card(fix) adds button and passes onClick props from Card to Button

--- src/components/Cards/CardLarge/CardLarge.js
+++ src/components/Cards/CardLarge/CardLarge.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
+// import { useDispatch } from 'react-redux';
 
+import Button from '../../Button/Button';
 import './CardLarge.scss';
 
-const CardLarge = ({ image, imageAlt, header, paragraph, button }) => {
+const CardLarge = ({ image, imageAlt, header, paragraph, button, onClick }) => {
+  const ref = useRef();
   return (
     <div className='card-large'>
-      <img className='card-large__image' src={image} alt={imageAlt}/>
+      <img className='card-large__image' src={image} alt={imageAlt} />
       <div className='card-large__text'>
         <p className='card-large__header'>{header}</p>
         <p className='card-large__paragraph'>{paragraph}</p>
       </div>
-      <button className='card-large__button'>{button}</button>
+      <div className='card-large__button'>
+        <Button type='filled' ref={ref} onClick={onClick}>
+          {button}
+        </Button>
+      </div>
     </div>
   );
 };
--- src/components/Cards/CardSmall/CardSmall.js
+++ src/components/Cards/CardSmall/CardSmall.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
+// import { useDispatch } from 'react-redux';
 
+import Button from '../../Button/Button';
 import './CardSmall.scss';
 
-const CardSmall = ({ image, imageAlt, header, paragraph, button }) => {
+const CardSmall = ({ image, imageAlt, header, paragraph, button, onClick }) => {
+  const ref = useRef();
   return (
     <div className='card-small'>
-      <img className='card-small__image' src={image} alt={`${imageAlt}`}/>
+      <img className='card-small__image' src={image} alt={`${imageAlt}`} />
       <div className='card-small__text'>
         <h1 className='card-small__header'>{header}</h1>
         <p className='card-small__paragraph'>{paragraph}</p>
       </div>
-      <button className='card-small__button'>{button}</button>
+      <button className='card-small__button'>
+        <Button type='text' ref={ref} onClick={onClick}>
+          {button}
+        </Button>
+      </button>
     </div>
   );
 };
