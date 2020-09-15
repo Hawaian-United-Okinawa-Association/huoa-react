2b2a0ce775f613b734f85efd108f157c199fcee3
jason-toups <me@jasontoups.com>
2020-09-15 12:04:38 -0700

    fix(card) updates card demo invokation

--- src/pages/Demo/Demo.js
+++ src/pages/Demo/Demo.js
@@ -11,7 +11,7 @@ import './Demo.scss';
 
 const Demo = () => {
   const dispatch = useDispatch();
-  const thankYou = <CustomThankYou />
+  const thankYou = <CustomThankYou />;
   const ref = useRef();
 
   return (
@@ -19,14 +19,32 @@ const Demo = () => {
       <h1>Demo Page</h1>
       <Container>
         <div className='demo__buttons'>
-          <Button type='filled' onClick={() => dispatch(openModal(thankYou))}>Opens Modal</Button>
-          <Button type='outlined' ref= {ref} onClick={() => console.log('hello')}>Say Hello</Button>
-          <Button type='text' onClick={() => console.log(ref.current.innerHTML = 'Changed Say Hello')} icon>Change Hello</Button>
-          <Button type='filled' link='https:www.google.com'>Ext Link (Google)</Button>
-          <Button type='outlined' link='/donate'>Int Link (Donate)</Button>
+          <Button type='filled' onClick={() => dispatch(openModal(thankYou))}>
+            Opens Modal
+          </Button>
+          <Button
+            type='outlined'
+            ref={ref}
+            onClick={() => console.log('hello')}>
+            Say Hello
+          </Button>
+          <Button
+            type='text'
+            onClick={() =>
+              console.log((ref.current.innerHTML = 'Changed Say Hello'))
+            }
+            icon>
+            Change Hello
+          </Button>
+          <Button type='filled' link='https:www.google.com'>
+            Ext Link (Google)
+          </Button>
+          <Button type='outlined' link='/donate'>
+            Int Link (Donate)
+          </Button>
         </div>
       </Container>
-      <CardDemo></CardDemo>
+      <CardDemo />
     </section>
   );
 };
