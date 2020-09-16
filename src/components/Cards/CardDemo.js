3df1d341a732419ffba79525b027e2df649a2155
jason-toups <me@jasontoups.com>
2020-09-15 17:39:15 -0700

    fix(card) remove unnecessary card divs

--- src/components/Cards/CardDemo.js
+++ src/components/Cards/CardDemo.js
@@ -18,14 +18,16 @@ const CardDemo = props => {
             header='Banquet Facility'
             paragraph='Description and it its a bit longer because it is giving more detailed information about the event. It will probably be a few sentences.'
             button='SCHEDULE'
-            onClick={() => console.log('you clicked a button')}></CardLarge>
+            onClick={() => console.log('you clicked a button')}
+          />
           <CardLarge
             image='https://huoa.org/nuuzi/banquetfacility/hoc.jpg'
             imageAlt='banquet facility'
             header='Banquet Facility'
             paragraph='Description and it its a bit longer because it is giving more detailed information about the event. It will probably be a few sentences.'
             button='SCHEDULE'
-            onClick={() => console.log('you clicked a button')}></CardLarge>
+            onClick={() => console.log('you clicked a button')}
+          />
         </Container>
       </div>
       <div className='card-demo--small'>
@@ -37,28 +39,32 @@ const CardDemo = props => {
             header='Headline Here'
             paragraph='Description and it its a pretty short. Nothing too long here.'
             button='Button Text'
-            onClick={() => console.log('You clicked a button')}></CardSmall>
+            onClick={() => console.log('You clicked a button')}
+          />
           <CardSmall
             image='https://huoa.org/nuuzi/data1/images/2018FestBonDanceGY.jpg'
             imageAlt='dancing'
             header='Headline Here'
             paragraph='Description and it its a pretty short. Nothing too long here.'
             button='Button Text'
-            onClick={() => console.log('You clicked a button')}></CardSmall>
+            onClick={() => console.log('You clicked a button')}
+          />
           <CardSmall
             image='https://huoa.org/nuuzi/data1/images/2018FestBonDanceGY.jpg'
             imageAlt='dancing'
             header='Headline Here'
             paragraph='Description and it its a pretty short. Nothing too long here.'
             button='Button Text'
-            onClick={() => console.log('You clicked a button')}></CardSmall>
+            onClick={() => console.log('You clicked a button')}
+          />
           <CardSmall
             image='https://huoa.org/nuuzi/data1/images/2018FestBonDanceGY.jpg'
             imageAlt='dancing'
             header='Headline Here'
             paragraph='Description and it its a pretty short. Nothing too long here.'
             button='Button Text'
-            onClick={() => console.log('You clicked a button')}></CardSmall>
+            onClick={() => console.log('You clicked a button')}
+          />
         </Container>
       </div>
       <div className='card-demo--square'>
@@ -69,19 +75,22 @@ const CardDemo = props => {
             subheader='Lorem Subhead Goes Here'
             paragraph='Description and it its a bit longer because it is giving more detailed information about the event.'
             button='LEARN MORE'
-            onClick={() => console.log('You clicked a button')}></CardSquare>
+            onClick={() => console.log('You clicked a button')}
+          />
           <CardSquare
             header='Kokua In Kind'
             subheader='Lorem Subhead Goes Here'
             paragraph='Description and it its a bit longer because it is giving more detailed information about the event.'
             button='LEARN MORE'
-            onClick={() => console.log('You clicked a button')}></CardSquare>
+            onClick={() => console.log('You clicked a button')}
+          />
           <CardSquare
             header='Kokua In Kind'
             subheader='Lorem Subhead Goes Here'
             paragraph='Description and it its a bit longer because it is giving more detailed information about the event.'
             button='LEARN MORE'
-            onClick={() => console.log('You clicked a button')}></CardSquare>
+            onClick={() => console.log('You clicked a button')}
+          />
         </Container>
       </div>
     </>
