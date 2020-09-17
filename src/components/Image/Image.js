8ed69f2a73388522f4f22cbd4b71bb1db40d69c7
Matt Laguardia <matthewlaguardia@gmail.com>
2020-09-17 15:57:38 -0700

    feat(image-component): creates image component for project

---
+++ src/components/Image/Image.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Image = ({ className, webp, img, alt }) => {
+  return (
+    <picture>
+      <source srcSet={ webp } type='image/webp' />
+      <img className={ className } src={ img } alt={ alt ? alt : 'Huoa.org Image' } />
+    </picture>
+  )
+};
+
+export default Image;
