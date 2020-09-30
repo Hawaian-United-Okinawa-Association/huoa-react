632f9a4f4fdc039de117dc502d0794aab32fd6ca
Marco Salcedo <masalcedo.88@gmail.com>
2020-09-30 16:21:27 -0700

    adds first iteration of mobilenav component and styles

---
+++ src/components/NavMobile/NavMobile.js
@@ -0,0 +1,78 @@
+//Dependencies
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+//Components, assets, actions, styles etc..
+import Button from '../Button/Button';
+import { ReactComponent as HUOALogo } from '../../assets/huoa-logo.svg';
+import { ReactComponent as Hamburger } from '../../assets/hamburger.svg';
+import { ReactComponent as ButtonClose } from '../../assets/button-close.svg';
+import { ReactComponent as Carrot } from '../../assets/carrot.svg';
+import './NavMobile.scss';
+
+const NavMobile = () => {
+  const [ navState, setNavState ] = useState(false);
+  const { routes } = useSelector((state) => state.router);
+
+  const mobileRoutes = routes.filter((item) => item.name !== 'Donate');
+  mobileRoutes.unshift({ name: 'Home', linkTo: '/' });
+
+  useEffect(() => {
+    console.log(`NavState is now ${navState}`);
+  });
+
+  const renderSideItems = (items) => {
+    return items.map((item) => {
+      return (
+        <Link
+          to={item.linkTo}
+          key={item.linkTo}
+          navState={navState}
+          setNavState={setNavState}
+          className="nav-mobile__sidebar--item"
+        >
+          <li>{item.name}</li>
+          {item.children ? <Carrot /> : null}
+        </Link>
+      );
+    });
+  };
+
+  const renderSideNav = () => {
+    return (
+      <div className="nav-mobile__sidebar">
+        <ButtonClose onClick={() => setNavState(!navState)} className="nav-mobile__sidebar--close" />
+        <ul className="nav-mobile__sidebar--items">{renderSideItems(mobileRoutes)}</ul>
+        <hr className="nav-mobile__sidebar--break" />
+        <div className="nav-mobile__sidebar--footer">
+          <small className="nav-mobile__sidebar--footer-link">Rent Our Ballroom</small>
+          <small className="nav-mobile__sidebar--footer-link">Join our Newsletter</small>
+          <div className="nav-mobile__sidebar--footer-button">
+            <Button type="filled" link="donate">
+              Donate
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <nav className="nav-mobile">
+        <div className="nav-mobile__header">
+          <HUOALogo height="90" width="90" />
+          <div className="nav-mobile__titles">
+            <h2 className="nav-mobile__title">Hawaii United Okinawa Association</h2>
+            <h5 className="nav-mobile__title--sm">Celebrating 120 years of Uchinanchu in Hawaii</h5>
+          </div>
+        </div>
+        <Hamburger onClick={() => setNavState(!navState)} />
+        {navState === true && renderSideNav()}
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default NavMobile;
