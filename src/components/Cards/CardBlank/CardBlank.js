6634787212068641df957dd4455bfcf3c165ab02
jason-toups <me@jasontoups.com>
2020-10-06 12:15:14 -0700

    fix(card): removed ref from card

--- src/components/Cards/CardBlank/CardBlank.js
+++ src/components/Cards/CardBlank/CardBlank.js
@@ -1,11 +1,10 @@
-import React, { useRef } from 'react';
+import React from 'react';
 
 // import Image from '../../Image/Image';
 // import Button from '../../Button/Button';
 import './CardBlank.scss';
 
 const CardBlank = ({ children }) => {
-  const ref = useRef();
   return <div className='card-blank'>{children}</div>;
 };
 
