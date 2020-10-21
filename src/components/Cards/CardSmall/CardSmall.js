67ab0493836f12a281df6ead62cd0434189ace88
jason-toups <me@jasontoups.com>
2020-10-21 16:22:37 -0700

    fix(card): adds border radius to card and image overflow hidden

--- src/components/Cards/CardSmall/CardSmall.js
+++ src/components/Cards/CardSmall/CardSmall.js
@@ -8,7 +8,7 @@ const CardSmall = ({ image, altTag, header, body, button, onClick }) => {
   const ref = useRef();
   return (
     <div className='card-small'>
-      <Image className='card-small__image' img={image} alt={altTag} />
+      <Image className='card-small__image' webp={image} alt={altTag} />
       <div className='card-small__text'>
         <h1 className='card-small__header'>{header}</h1>
         <p className='card-small__body'>{body}</p>
