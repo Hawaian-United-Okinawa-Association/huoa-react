1bde06b041d3bc732409eede12f04217723d66a5
Ryan <ryansherring@gmail.com>
2020-11-03 13:49:01 -0800

    fixes link hover

--- src/pages/GetInvolved/GetInvolved.js
+++ src/pages/GetInvolved/GetInvolved.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import Axios from "axios";
 import './GetInvolved.scss';
 
-//QUESTIONS: OnHover what do? sidebar? pagination? new api call? column breakpoints?
 
 
 var APIClubRoute = `https://dev.huoa.org/wp-json/wp/v2/clubs`;
@@ -12,7 +11,6 @@ var APIClubRoute = `https://dev.huoa.org/wp-json/wp/v2/clubs`;
 const GetInvolved = () => {
   const [items, setItems] = useState([]);
 
-  //using useEffect to preempt pagination
   useEffect(() => {
     Axios.get(APIClubRoute).then((res) => {
       setItems(res.data);
@@ -33,7 +31,7 @@ const GetInvolved = () => {
             {items.map((item) => {
               return (
                 <li className={item}>
-                  <Link key={item.id} to={item.link}>
+                  <Link className="getinvolved__items--link" key={item.id} to={item.link}>
                     {item.slug}
                   </Link>
                 </li>
