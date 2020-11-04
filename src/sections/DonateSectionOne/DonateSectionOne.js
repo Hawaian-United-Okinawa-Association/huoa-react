5aeee4324c6e6239fbb650fd04c713cf45184ecc
Michael <mcheng.oc@gmail.com>
2020-11-03 21:54:12 -0800

    update(donate-section-one):styles component

--- src/sections/DonateSectionOne/DonateSectionOne.js
+++ src/sections/DonateSectionOne/DonateSectionOne.js
@@ -4,15 +4,20 @@ import Button from 'components/Button/Button';
 import './DonateSectionOne.scss';
 
 const DonateSectionOne = ({ data }) => {
-  console.log(data);
+  
+  function createTextMarkup() {
+    return {__html: data.text};
+  }
+
   return (
     <div className='donate-section-one'>
-      <h1>{data.title}</h1>
-      <div>
-        <div>
-          <p>{data.text}</p>
-          <Button link={data.button_link}>{data.button_text}</Button>
+      <h1 className='donate-section-one__title'>{data.title}</h1>
+      <div className='donate-section-one__body'>
+        <div className='donate-section-one__content'>
+          <div className='donate-section-one__text' dangerouslySetInnerHTML={createTextMarkup()} />
+          <Button link={data.button_link} type='filled'>{data.button_text}</Button>
         </div>
+        <img className='donate-section-one__image' src={data.image_link} alt='Giving To HUOA'/>
       </div>
     </div>
   )
