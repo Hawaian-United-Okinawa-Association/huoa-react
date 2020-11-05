e8289ec4a8ac6f18d6b3a0c3db37b25ec4599c9e
Ryan <ryansherring@gmail.com>
2020-11-04 19:02:39 -0800

    desktop styling

--- src/actions/ClubActions.js
+++ src/actions/ClubActions.js
@@ -5,7 +5,5 @@ const api = 'https://dev.huoa.org/wp-json';
 
 export const getClubs = () => async (dispatch) => {
     const response = await axios.get(`${api}/wp/v2/clubs`);
-    // const data = response.data.reduce((allData, { slug, title, acf }) => ({ ...allData, [slug]: { title: title.rendered, ...acf }}), {});
-    // console.log(response)
     dispatch({ type: FETCH_CLUBS, payload: response.data });
   };
--- src/pages/GetInvolved/GetInvolved.js
+++ src/pages/GetInvolved/GetInvolved.js
@@ -2,13 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux"
 import Layout from "components/Layout/Layout";
 import { Link } from "react-router-dom";
-// import Axios from "axios";
 import './GetInvolved.scss';
-import { render } from "@testing-library/react";
-
-
-
-
 
 const GetInvolved = () => {
   // const [items, setItems] = useState([]);
@@ -30,7 +24,7 @@ const GetInvolved = () => {
     return clubs.map((club) => {
       return (
         <li>
-          <Link className="getinvolved__items--link" key={club.id} to={club.link}>
+          <Link className="get-involved__items--link" key={club.id} to={club.link}>
             {club.title.rendered}
           </Link>
         </li>
@@ -40,15 +34,15 @@ const GetInvolved = () => {
 
   return (
     <Layout>
-      <div className="get-involved__mainContainer">
+      <div className="get-involved__container">
           <div className="get-involved__title">
             <h2>HUOA Club Listing</h2>
           </div>
           <div className="get-involved__description">
             <p2>Please contact HUOA if you are interested in joining and finding out more about the HUOA clubs.  Contact us at 676-5400.</p2>
           </div>
-        <div className="getinvolved__bodycontainer">
-          <ul className="getinvolved__items">
+        <div className="get-involved__body">
+          <ul className="get-involved__items">
             {renderClubs(clubDataAll)}
           </ul>
         </div>
