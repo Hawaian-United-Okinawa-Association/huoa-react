86c9b5fac638a72367a5c09c3a39ba5ede02090c
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-11-10 13:53:17 -0800

    removed layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,21 +2,13 @@ import React from 'react'
 
 import './Layout.scss'
 
-const Layout = ({ sideNav, children }) => {
-  if (sideNav) {
-    return (
+const Layout = ({ children }) => {
+  return (
       <div className='layout'>
         <div className='layout__sidenav'>Sidebar Component</div>
         <div className='layout__body'>{children}</div>
       </div>
     )
-  } else {
-    return (
-      <div className='layout'>
-        <div className='layout__full'>{children}</div>
-      </div>
-    )
-  }
-}
+};
 
 export default Layout;
