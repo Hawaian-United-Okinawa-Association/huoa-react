149ee7dae9b7bde8edabf90752cfba0327cc7a69
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-11-11 14:00:57 -0800

    switched props render, now uses sidenav by default

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 import './Layout.scss'
 
-const Layout = ({ sideNav, children }) => {
-  if (sideNav) {
+const Layout = ({ noNav, children }) => {
+  if (noNav) {
     return (
       <div className='layout'>
-        <div className='layout__sidenav'>Sidebar Component</div>
-        <div className='layout__body'>{children}</div>
+        <div className='layout__full'>{children}</div>
       </div>
     )
   } else {
     return (
       <div className='layout'>
-        <div className='layout__full'>{children}</div>
+        <div className='layout__sidenav'>Sidebar Component</div>
+        <div className='layout__body'>{children}</div>
       </div>
     )
   }
