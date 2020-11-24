b9a1f2fb5fab9b3ef75f221cfc8d64132847ee10
Matt Laguardia <matthewlaguardia@gmail.com>
2020-11-23 16:54:36 -0800

    feat(sidebar): creates new sidebar component

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from 'components/Layout/Layout';
 
 const Events = () => {
   return (
-    <div>
+    <Layout>
       <h4>Events</h4>
-    </div>
+    </Layout>
   );
 };
 
