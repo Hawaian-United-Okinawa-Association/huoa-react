0e4c77c3f1939a3f27b6dd827dd2adbff5533ec4
Gordon Lewis <glewis@berkeley.edu>
2020-11-27 17:18:33 -0800

    cleaned up banquet facility.scss and the banquetfacility.js to match the typography.scss

--- src/pages/BanquetFacility/BanquetFacility.js
+++ src/pages/BanquetFacility/BanquetFacility.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import Layout from 'components/Layout/Layout.js';
 import Container from 'components/Container/Container.js';
-import './BanquetFacility.scss';
 import Button from 'components/Button/Button.js';
 
 function createMarkup(body) {
@@ -13,24 +13,24 @@ const BanquetFacility = ({data}) => {
   let {section_one, section_two} = data.banquet_facility_page;
 
   return(
-    <Container>
-    <div className="banquet-facility">
-      <h1 className="banquet-facility__title">{section_one.title}</h1>
-      <h1 className="banquet-facility__subtitle">{section_one.subtitle}</h1>
-      <div className="banquet-facility__body" dangerouslySetInnerHTML = {createMarkup(section_one.body)}/>
-      <h1 className="banquet-facility__section-heading">{section_two.title}</h1>
-      <div className="banquet-facility__container">
-        <img className="banquet-facility__image" src={section_two.image} alt=""/>
-        {/* not sure how to do this semantically properly with BEM*/}
-        <div className="banquet-facility__section">
-          <div className="banquet-facility__body--right" dangerouslySetInnerHTML= {createMarkup(section_two.body)}/>
-          <Button type="filled" link={section_two.button.link}>{section_two.button.text}</Button>
+    <Layout>
+      <Container>
+      <div className="banquet-facility">
+        <h2 className="banquet-facility__title">{section_one.title}</h2>
+        <h4 className="banquet-facility__subtitle">{section_one.subtitle}</h4>
+        <div className="banquet-facility__body" dangerouslySetInnerHTML = {createMarkup(section_one.body)}/>
+        <h1 className="banquet-facility__section-heading">{section_two.title}</h1>
+        <div className="banquet-facility__container">
+          <img className="banquet-facility__image" src={section_two.image} alt=""/>
+          <div className="banquet-facility__section">
+            <div className="banquet-facility__body--right" dangerouslySetInnerHTML= {createMarkup(section_two.body)}/>
+            <Button type="filled" link={section_two.button.link}>{section_two.button.text}</Button>
+          </div>
         </div>
       </div>
-    </div>
-    </Container>
-   
+      </Container>     
+    </Layout>
   )
 }
 
-export default BanquetFacility
+export default BanquetFacility;
