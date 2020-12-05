e75b03e7edd3fe0ac0c2038b8b9e9be636f214cc
Gordon Lewis <glewis@berkeley.edu>
2020-12-04 16:58:57 -0800

    added the 3 photos on top of banquet facility, also added styling to section on the right

--- src/pages/BanquetFacility/BanquetFacility.js
+++ src/pages/BanquetFacility/BanquetFacility.js
@@ -3,6 +3,10 @@ import Layout from 'components/Layout/Layout.js';
 import Container from 'components/Container/Container.js';
 import Button from 'components/Button/Button.js';
 import Image from 'components/Image/Image.js';
+import bfimg1 from 'assets/banquet-facility1.jpg';
+import bfimg2 from 'assets/banquet-facility2.jpg';
+import bfimg3 from 'assets/banquet-facility3.jpg';
+
 function createMarkup(body) {
   return {__html: body};
   }
@@ -16,6 +20,11 @@ const BanquetFacility = ({data}) => {
     <Layout>
       <Container>
       <div className="banquet-facility">
+        <div className="banquet-facility__triple">
+          <Image className="banquet-facility__image--triple" img={bfimg1} alt=""></Image>
+          <Image className="banquet-facility__image--triple" img={bfimg2} alt=""></Image>
+          <Image className="banquet-facility__image--triple" img={bfimg3} alt=""></Image>
+        </div>
         <h2 className="banquet-facility__title">{section_one.title}</h2>
         <h4 className="banquet-facility__subtitle">{section_one.subtitle}</h4>
         <div className="banquet-facility__body" dangerouslySetInnerHTML = {createMarkup(section_one.body)}/>
