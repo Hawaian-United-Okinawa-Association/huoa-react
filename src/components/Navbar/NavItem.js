20ef4ec18cd6c100784d14e6c3c01ae42596fbe7
Marco Salcedo <masalcedo.88@gmail.com>
2020-12-07 14:25:35 -0800

    refactor navitem to use switch, adds redirect to shop, small style changes

--- src/components/Navbar/NavItem.js
+++ src/components/Navbar/NavItem.js
@@ -9,24 +9,61 @@ const NavItem = (props) => {
   const { name, linkTo, children, navState, setNavState } = props;
   const isActive = navState === name ? 'navbar__link--active' : null;
 
-  return name === 'Donate' ? (
-    <Button type="outlined" link="donate">
-      Donate
-    </Button>
-  ) : (
-    <div>
-      <Link
-        className={`navbar__link ${isActive}`}
-        to={linkTo}
-        data-text={name}
-        onMouseEnter={() => setNavState(name)}
-        onClick={() => setNavState(false)}
-      >
-        {name}
-      </Link>
-      {navState === name && children}
-    </div>
-  );
+  const donateItem = () => {
+    return (
+      <div onMouseEnter={() => setNavState(null)}>
+        <Button type="outlined" link="donate">
+          Donate
+        </Button>
+      </div>
+    );
+  };
+
+  const redirectItem = (name, linkTo, isActive) => {
+    return (
+      <div>
+        <a className={`navbar__link ${isActive}`} href={linkTo} data-text={name} onMouseEnter={() => setNavState(name)}>
+          {name}
+        </a>
+      </div>
+    );
+  };
+
+  const standardItem = (name, linkTo, children, isActive) => {
+    if (children) {
+      return (
+        <div>
+          <div className={`navbar__link ${isActive}`} data-text={name} onMouseEnter={() => setNavState(name)}>
+            {name}
+          </div>
+          {navState === name && children}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link
+            className={`navbar__link ${isActive}`}
+            to={linkTo}
+            data-text={name}
+            onMouseEnter={() => setNavState(name)}
+            onClick={() => setNavState(false)}
+          >
+            {name}
+          </Link>
+        </div>
+      );
+    }
+  };
+
+  switch (name) {
+    case 'Donate':
+      return donateItem();
+    case 'Shop':
+      return redirectItem(name, linkTo, isActive);
+    default:
+      return standardItem(name, linkTo, children, isActive);
+  }
 };
 
 export default NavItem;
