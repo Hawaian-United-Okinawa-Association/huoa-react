a7e0a42865e1dc0615935fc68bdec3c4ee282df2
cooperama <coope133@gmail.com>
2021-01-04 18:51:18 -0800

    Implements catch for errors on api calls

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,10 +10,14 @@ export const getPages = () => async (dispatch) => {
 
     dispatch({ type: GET_PAGES, payload: cache.pages });
   } else {
-    const response = await axios.get(`${api}/wp/v2/pages?page=1&per_page=100`);
-    const data = response.data.reduce((allData, { slug, title, acf }) => ({ ...allData, [slug]: { title: title.rendered, ...acf }}), {});
-
-    dispatch({ type: GET_PAGES, payload: data });
+    try {
+      const response = await axios.get(`${api}/wp/v2/pages?page=1&per_page=100`);
+      const data = response.data.reduce((allData, { slug, title, acf }) => ({ ...allData, [slug]: { title: title.rendered, ...acf }}), {});
+  
+      dispatch({ type: GET_PAGES, payload: data });
+    } catch (error) {
+      console.log(error)
+    }
   }
 };
 
@@ -23,9 +27,13 @@ export const getClubs = () => async (dispatch) => {
 
     dispatch({ type: FETCH_CLUBS, payload: cache.clubs })
   } else {
-    const response = await axios.get(`${api}/wp/v2/clubs?&page=1&per_page=100&orderby=slug&order=asc`);
-
-    dispatch({ type: FETCH_CLUBS, payload: response.data });
+    try {
+      const response = await axios.get(`${api}/wp/v2/clubs?&page=1&per_page=100&orderby=slug&order=asc`);
+  
+      dispatch({ type: FETCH_CLUBS, payload: response.data });
+    } catch (error) {
+      console.log(error)
+    }
   }
 };
 
@@ -35,9 +43,13 @@ export const getHeros = () => async (dispatch) => {
 
     dispatch({ type: GET_HEROS, payload: cache.heros });
   } else {
-    const { data } = await axios.get(`${api}/wp/v2/heros?page=1&per_page=100`);
-
-    dispatch({ type: GET_HEROS, payload: data });
+    try {
+      const { data } = await axios.get(`${api}/wp/v2/heros?page=1&per_page=100`);
+  
+      dispatch({ type: GET_HEROS, payload: data });
+    } catch (error) {
+      console.log(error)
+    }
   }
 };
 
@@ -47,9 +59,13 @@ export const getEvents = () => async (dispatch) => {
 
     dispatch({ type: GET_EVENTS, payload: cache.events });
   } else {
-    const { data } = await axios.get(`${api}/wp/v2/events?page=1&per_page=100`);
-
-    dispatch({ type: GET_EVENTS, payload: data });
+    try {
+      const { data } = await axios.get(`${api}/wp/v2/events?page=1&per_page=100`);
+  
+      dispatch({ type: GET_EVENTS, payload: data });
+    } catch (error) {
+      console.log(error)
+    }
   }
 };
 
