ab3ef8ebc0b89676776ba1a214007a98cfc9d5f4
cooperama <coope133@gmail.com>
2021-01-12 17:06:39 -0800

    Sets shadows to variables

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,70 +1,103 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { FETCH_CLUBS, GET_PAGES, GET_HEROS, GET_EVENTS, MODAL_OPEN, MODAL_CLOSE } from './actionTypes';
+import {
+  FETCH_CLUBS,
+  GET_PAGES,
+  GET_HEROS,
+  GET_EVENTS,
+  MODAL_OPEN,
+  MODAL_CLOSE,
+} from "./actionTypes";
 
-const api = 'https://dev.huoa.org/wp-json';
+const api = "https://dev.huoa.org/wp-json";
 
 export const getPages = () => async (dispatch) => {
-  if (process.env.NODE_ENV === "production" && navigator.userAgent !== "ReactSnap") {
+  if (
+    process.env.NODE_ENV === "production" &&
+    navigator.userAgent !== "ReactSnap"
+  ) {
     let cache = window.__REDUX_STATE__;
 
     dispatch({ type: GET_PAGES, payload: cache.pages });
   } else {
     try {
-      const response = await axios.get(`${api}/wp/v2/pages?page=1&per_page=100`);
-      const data = response.data.reduce((allData, { slug, title, acf }) => ({ ...allData, [slug]: { title: title.rendered, ...acf }}), {});
-  
+      const response = await axios.get(
+        `${api}/wp/v2/pages?page=1&per_page=100`
+      );
+      const data = response.data.reduce(
+        (allData, { slug, title, acf }) => ({
+          ...allData,
+          [slug]: { title: title.rendered, ...acf },
+        }),
+        {}
+      );
+
       dispatch({ type: GET_PAGES, payload: data });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
 };
 
 export const getClubs = () => async (dispatch) => {
-  if (process.env.NODE_ENV === "production" && navigator.userAgent !== "ReactSnap") {
+  if (
+    process.env.NODE_ENV === "production" &&
+    navigator.userAgent !== "ReactSnap"
+  ) {
     let cache = window.__REDUX_STATE__;
 
-    dispatch({ type: FETCH_CLUBS, payload: cache.clubs })
+    dispatch({ type: FETCH_CLUBS, payload: cache.clubs });
   } else {
     try {
-      const response = await axios.get(`${api}/wp/v2/clubs?&page=1&per_page=100&orderby=slug&order=asc`);
-  
+      const response = await axios.get(
+        `${api}/wp/v2/clubs?&page=1&per_page=100&orderby=slug&order=asc`
+      );
+
       dispatch({ type: FETCH_CLUBS, payload: response.data });
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
   }
 };
 
 export const getHeros = () => async (dispatch) => {
-  if (process.env.NODE_ENV === "production" && navigator.userAgent !== "ReactSnap") {
+  if (
+    process.env.NODE_ENV === "production" &&
+    navigator.userAgent !== "ReactSnap"
+  ) {
     let cache = window.__REDUX_STATE__;
 
     dispatch({ type: GET_HEROS, payload: cache.heros });
   } else {
     try {
-      const { data } = await axios.get(`${api}/wp/v2/heros?page=1&per_page=100`);
-  
+      const { data } = await axios.get(
+        `${api}/wp/v2/heros?page=1&per_page=100`
+      );
+
       dispatch({ type: GET_HEROS, payload: data });
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
   }
 };
 
 export const getEvents = () => async (dispatch) => {
-  if (process.env.NODE_ENV === "production" && navigator.userAgent !== "ReactSnap") {
+  if (
+    process.env.NODE_ENV === "production" &&
+    navigator.userAgent !== "ReactSnap"
+  ) {
     let cache = window.__REDUX_STATE__;
 
     dispatch({ type: GET_EVENTS, payload: cache.events });
   } else {
     try {
-      const { data } = await axios.get(`${api}/wp/v2/events?page=1&per_page=100`);
-  
+      const { data } = await axios.get(
+        `${api}/wp/v2/events?page=1&per_page=100`
+      );
+
       dispatch({ type: GET_EVENTS, payload: data });
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
   }
 };
@@ -72,12 +105,12 @@ export const getEvents = () => async (dispatch) => {
 export const openModal = (modalContent) => {
   return {
     type: MODAL_OPEN,
-    payload: modalContent
+    payload: modalContent,
   };
 };
 
 export const closeModal = () => {
   return {
-    type: MODAL_CLOSE
+    type: MODAL_CLOSE,
   };
 };
