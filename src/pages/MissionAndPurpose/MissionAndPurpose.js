c3bf214e0f948a5b058d679ddfe25f7d31850ad3
Matt Laguardia <matthewlaguardia@gmail.com>
2021-01-18 15:39:17 -0800

    fix(mission-and-purpose): small fix to get builder to work

--- src/pages/MissionAndPurpose/MissionAndPurpose.js
+++ src/pages/MissionAndPurpose/MissionAndPurpose.js
@@ -10,17 +10,18 @@ function createMarkup(body) {
 const MissionAndPurpose = ({data}) => {
   if (!data) return null;
 
-  let {section_one, section_two, section_three} = data.mission_and_purpose;
+  let { builder } = data;
+
   return(
     <Layout>
       <Container>
         <div className="about-mission-purpose">
-          <h2 className="about-mission-purpose__title">{section_one.title}</h2>
-          <div className="about-mission-purpose__body" dangerouslySetInnerHTML = {createMarkup(section_one.body)}/>
-          <h2 className="about-mission-purpose__title">{section_two.title}</h2>
-          <div className="about-mission-purpose__body" dangerouslySetInnerHTML = {createMarkup(section_two.body)}/>
-          <h2 className="about-mission-purpose__title">{section_three.title}</h2>
-          <div className="about-mission-purpose__body" dangerouslySetInnerHTML = {createMarkup(section_three.body)}/>
+          <h2 className="about-mission-purpose__title">{builder[0].title_body.title}</h2>
+          <div className="about-mission-purpose__body" dangerouslySetInnerHTML = {createMarkup(builder[0].title_body.body)}/>
+          <h2 className="about-mission-purpose__title">{builder[1].title_body.title}</h2>
+          <div className="about-mission-purpose__body" dangerouslySetInnerHTML = {createMarkup(builder[1].title_body.body)}/>
+          <h2 className="about-mission-purpose__title">{builder[2].title_body.title}</h2>
+          <div className="about-mission-purpose__body" dangerouslySetInnerHTML = {createMarkup(builder[2].title_body.body)}/>
         </div>
       </Container>
     </Layout>
