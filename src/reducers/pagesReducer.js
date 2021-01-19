039b0aaf632ec5c2b414ca55c09b0991dc57c82a
cooperama <coope133@gmail.com>
2021-01-19 15:07:58 -0800

    Fixes getPagesReducer function

--- src/reducers/pagesReducer.js
+++ src/reducers/pagesReducer.js
@@ -4,14 +4,13 @@ import { createReducer } from './reducerUtils';
 const INITIAL_STATE = {};
 
 const getPagesReducer = (state, payload) => {
-  payload.reduce(
+  return payload.reduce(
     (allData, { slug, title, acf }) => ({
       ...allData,
       [slug]: { title: title.rendered, ...acf },
     }),
     {}
   );
-  return payload;
 };
 
 export default createReducer(INITIAL_STATE, {
