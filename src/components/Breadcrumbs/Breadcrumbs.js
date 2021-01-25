b583a4c079b5e0b41e696289740cef951ffcd1f7
cooperama <coope133@gmail.com>
2021-01-25 14:24:28 -0800

    Removes Event breadcrumb styles, removes relative path for import, fixes spacing in Breadcrumbs component

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -4,14 +4,9 @@ import { Link } from "react-router-dom";
 const Breadcrumbs = ({ parent, parentText, rendered }) => {
   return (
     <p className="breadcrumb">
-    <Link className="breadcrumb__item" to="/">
-      Home
-    </Link>
-    /
-    <Link className="breadcrumb__item" to={ `/${parent}` } >
-      { parentText }
-    </Link>
-    /<span className="breadcrumb__item breadcrumb__item--selected">{rendered}</span>
+    <Link className="breadcrumb__item" to="/">Home</Link>/
+    <Link className="breadcrumb__item" to={ `/${parent}` } >{ parentText }</Link>/
+    <span className="breadcrumb__item breadcrumb__item--selected">{rendered}</span>
   </p>
   )
 }
