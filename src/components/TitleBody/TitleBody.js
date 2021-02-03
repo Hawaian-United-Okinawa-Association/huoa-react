543d18d44e34bb45089599ddfb121214688066ed
Gordon Lewis <glewis@berkeley.edu>
2021-02-03 14:42:02 -0800

    Update src/components/TitleBody/TitleBody.js
    Co-authored-by: Matt Laguardia <matthewlaguardia@gmail.com>

--- src/components/TitleBody/TitleBody.js
+++ src/components/TitleBody/TitleBody.js
@@ -3,10 +3,10 @@ import React from 'react';
 const TitleBody = ({ title, body }) => {
   
     return (
-        <React.Fragment>
+        <>
             <h2 className="title">{title}</h2>
             <div className="body" dangerouslySetInnerHTML = {{ __html: body }} />
-        </React.Fragment>
+        </>
         )
     }
 
