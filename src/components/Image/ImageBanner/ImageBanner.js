9d3e586e10337a22499f083450e8fe4062faa224
Gordon <glewis@berkeley.edu>
2021-02-12 12:18:03 -0800

    Change typography, Componentize Elements on banquet-facility and about-mission, style for mobile

---
+++ src/components/Image/ImageBanner/ImageBanner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from 'components/Image/Image.js';
+
+function ImageBanner({ image_banner }) {
+   return (
+    <div className="image-banner">
+      {image_banner.map((el, i) =>  
+       <Image key={i} className="image-banner__image" img={el.url} alt={el.alt}></Image>      
+      )}
+    </div>
+   );
+} 
+
+export default ImageBanner;
--- src/pages/BanquetFacility/BanquetFacility.js
+++ src/pages/BanquetFacility/BanquetFacility.js
@@ -1,30 +1,12 @@
-/*****************************
-  TODO: Refactor to use builder data instead
-  ###
-  First console.log(data); to see how the data is structured
-  Our new data is going to live under 'builder'
-  Second lets componentize these sections now, we should end up with 3 components
-    1. Image Banner
-    2. Title Subtitle Body
-    3. Image Title Body Button
-  NOTE: These components should be placed in folder name with leading name, exampled Title Subtitle Body should live in a title directory
-  Lastly, please delete this comment
-******************************/
-
 import React from 'react';
 import Layout from 'components/Layout/Layout.js';
 import Container from 'components/Container/Container.js';
-import Button from 'components/Button/Button.js';
-import Image from 'components/Image/Image.js';
-
-function createMarkup(body) {
-  return {__html: body};
-  }
+import ImageBanner from 'components/Image/ImageBanner/ImageBanner.js';
+import ImageTitleBodyButton from 'components/Image/ImageTitleBodyButton/ImageTitleBodyButton.js';
+import TitleSubtitleBody from 'components/Title/TitleSubtitleBody/TitleSubtitleBody.js';
 
 const BanquetFacility = ({ data }) => {
-
 if (!data) return null;
-
 let { builder } = data;
 let [image_banner, title_subtitle_body, image_title_body_button] = builder;
 
@@ -36,22 +18,20 @@ image_title_body_button= image_title_body_button.image_title_body_button;
     <Layout>
       <Container>
         <div className="banquet-facility">
-          <div className="banquet-facility__triple">
-          {image_banner.map((el, i) =>  {
-            return <Image key={i} className="banquet-facility__image--triple" img={el.url} alt={el.alt}></Image>      
-          })}
-          </div>
-          <h2 className="banquet-facility__title--page">{title_subtitle_body.title}</h2>
-          <h4 className="banquet-facility__subtitle">{title_subtitle_body.subtitle}</h4>
-          <div className="banquet-facility__body" dangerouslySetInnerHTML = {createMarkup(title_subtitle_body.body)}/>
-          <h1 className="banquet-facility__section-heading">{image_title_body_button.title}</h1>
-          <div className="banquet-facility__container">
-            <Image className="banquet-facility__image" img={image_title_body_button.image.url} alt="" />
-            <div className="banquet-facility__section">
-              <div className="banquet-facility__body--right" dangerouslySetInnerHTML= {createMarkup(image_title_body_button.body)}/>
-              <Button type="filled" link={image_title_body_button.button.link}>{image_title_body_button.button.text}</Button>
-            </div>
-          </div> 
+          <ImageBanner 
+            image_banner = {image_banner}
+          />
+          <TitleSubtitleBody 
+            title = {title_subtitle_body.title}
+            subtitle = {title_subtitle_body.subtitle}
+            body = {title_subtitle_body.body}
+          />
+          <ImageTitleBodyButton
+            image = {image_title_body_button.image}
+            title = {image_title_body_button.title}
+            body = {image_title_body_button.body}
+            button = {image_title_body_button.button}
+          />
         </div>
       </Container>
     </Layout>
