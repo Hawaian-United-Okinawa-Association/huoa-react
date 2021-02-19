db420c991a36caccfa50fdc60bc87c0fdeb0b8b7
Gordon Lewis <glewis@berkeley.edu>
2021-02-19 11:45:07 -0800

    Apply suggestions from code review
    Co-authored-by: Matt Laguardia <matthewlaguardia@gmail.com>

--- src/components/TitleBody/TitleBody.js
+++ src/components/TitleBody/TitleBody.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const TitleBody = ({ title, body }) => { 
   return (
-      <>
-          <h2 className="title-body__title">{title}</h2>
-          <div className="title-body__body" dangerouslySetInnerHTML = {{ __html: body }} />
-      </>
-    )
+    <>
+      <h2 className="title-body__title">{title}</h2>
+      <div className="title-body__body" dangerouslySetInnerHTML = {{ __html: body }} />
+    </>
+  )
 }
 
 export default TitleBody;
