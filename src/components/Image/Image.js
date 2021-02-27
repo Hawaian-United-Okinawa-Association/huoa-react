fcc63666d35bb2ea2c9722e2dc94d4706e79597c
ryansherring <ryansherring@gmail.com>
2021-02-27 14:28:29 -0800

    PR fixes

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Image = ({ className, webp, img, alt }) => {
   return (
-    <picture className="picture">
+    <picture>
       <source srcSet={ webp } type='image/webp' />
-      <img className={`picture__${className}`} src={ img } loading='lazy' alt={ alt ? alt : 'Huoa.org Image' } />
+      <img className={`${className}`} src={ img } loading='lazy' alt={ alt ? alt : 'Huoa.org Image' } />
     </picture>
   )
 };
