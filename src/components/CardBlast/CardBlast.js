84e8f75999f5d15210d43ca400b72880f476fe82
jason-toups <me@jasontoups.com>
2021-03-02 15:07:54 -0800

    create

---
+++ src/components/CardBlast/CardBlast.js
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+
+import Button from '../Button/Button';
+
+const CardBlast = ({ props }) => {
+  const { title, body, firstName, lastName, email, button } = props;
+  const ref = useRef();
+
+  return (
+    <div className='card-blast'>
+      <div className='card-blast__text'>
+        <h3 className='card-blast__title'>{ title }</h3>
+        <p className='card-blast__body'>{ body }</p>
+      </div>
+      <div className='card-blast__form'>
+        <input className='card-blank__input' type='text' placeholder={ firstName }></input>
+        <input className='card-blank__input' type='text' placeholder={ lastName }></input>
+        <input className='card-blank__input' type='text' placeholder={ email }></input>
+      </div>
+      <div className='card-blast__button'>
+        <Button type='filled' onClick={() => console.log('You clicked a button')}>{ button.text }</Button>
+      </div>
+    </div>
+  );
+};
+
+export default CardBlast;
