df3b0c75ff963f138c16827d27caf5910d5b02b3
jason-toups <me@jasontoups.com>
2021-03-03 20:54:14 -0800

    adds todo and updates props

--- src/components/CardBlast/CardBlast.js
+++ src/components/CardBlast/CardBlast.js
@@ -3,8 +3,12 @@ import React, { useRef } from 'react';
 import Button from '../Button/Button';
 
 const CardBlast = ({ props }) => {
-  const { title, body, firstName, lastName, email, button, api } = props;
+  const { title, body, firstName, lastName, email, buttonText, subscribeUrl } = props;
   const ref = useRef();
+
+  //TODO - build the api request function, using the api from wordpress. Then check the return, and start building the logic if the email already exists in the return, then show a confirmation on the form. Probably over the email field.
+
+  // let apiPlaceholder = 'https://huoa.us10.list-manage.com/subscribe/post?u=fb4e3b9e255496f68e4d87e35&amp;id=96858fbc3b';
 
   return (
     <div className='card-blast'>
@@ -18,7 +22,7 @@ const CardBlast = ({ props }) => {
         <input className='card-blast__input' type='text' placeholder={ email }></input>
       </div>
       <div className='card-blast__button'>
-        <Button type='filled' onClick={() => console.log(`here's the api { api }`)}>{ button.text }</Button>
+        <Button type='filled' ref={ ref } onClick={() => console.log(`here's the api ${ subscribeUrl }`)}>{ buttonText }</Button>
       </div>
     </div>
   );
