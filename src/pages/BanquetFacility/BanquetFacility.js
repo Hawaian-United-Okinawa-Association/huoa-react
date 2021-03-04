28578df050222551d2ba8f0cc01a7b6ea586829c
Matt Laguardia <matthewlaguardia@gmail.com>
2021-03-04 09:22:52 -0800

    fixes(page): banquet page

--- src/pages/BanquetFacility/BanquetFacility.js
+++ src/pages/BanquetFacility/BanquetFacility.js
@@ -6,31 +6,27 @@ import ImageTitleBodyButton from 'components/ImageTitleBodyButton/ImageTitleBody
 import TitleSubtitleBody from 'components/TitleSubtitleBody/TitleSubtitleBody.js';
 
 const BanquetFacility = ({ data }) => {
-if (!data) return null;
-let { builder } = data;
-let [image_banner, title_subtitle_body, image_title_body_button] = builder;
-
-image_banner = image_banner.image_banner;
-title_subtitle_body = title_subtitle_body.title_subtitle_body;
-image_title_body_button= image_title_body_button.image_title_body_button;
+  if (!data) return null;
+  let { builder } = data;
+  let [image_banner, title_subtitle_body, image_title_body_button] = builder;
 
   return(
     <Layout>
       <Container>
         <div className="banquet-facility">
-          <ImageBanner 
-            image_banner = {image_banner}
+          <ImageBanner
+            images={ image_banner.image_banner.images }
           />
-          <TitleSubtitleBody 
-            title = {title_subtitle_body.title}
-            subtitle = {title_subtitle_body.subtitle}
-            body = {title_subtitle_body.body}
+          <TitleSubtitleBody
+            title = {title_subtitle_body.title_subtitle_body.title}
+            subtitle = {title_subtitle_body.title_subtitle_body.subtitle}
+            body = {title_subtitle_body.title_subtitle_body.body}
           />
           <ImageTitleBodyButton
-            image = {image_title_body_button.image}
-            title = {image_title_body_button.title}
-            body = {image_title_body_button.body}
-            button = {image_title_body_button.button}
+            image = {image_title_body_button.image_title_body_button.image}
+            title = {image_title_body_button.image_title_body_button.title}
+            body = {image_title_body_button.image_title_body_button.body}
+            button = {image_title_body_button.image_title_body_button.button}
           />
         </div>
       </Container>
