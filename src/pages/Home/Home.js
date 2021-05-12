8a3ea8276a8ab884afd7c2db6bb83339ddfff83c
jason-toups <me@jasontoups.com>
2021-05-12 16:48:27 -0700

    updates id to class

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -29,7 +29,7 @@ const Home = ({ data }) => {
         </Container>
       </section>
 
-      <section className="home__section" id="community">
+      <section className="home__section home__section__community">
         <Container col='1'>
           <h2 className="home__title">Support Your Community</h2>
         </Container>
