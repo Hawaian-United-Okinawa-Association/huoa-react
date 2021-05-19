6d0aeea4c769669a37e836890a023c8de601f577
Ryan Sherring <35449332+ryansherring@users.noreply.github.com>
2021-05-19 09:58:53 -0700

    Google analytics (#117)
    * google analytics
    
    * analytics

--- src/config/header.js
+++ src/config/header.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+const GoogleAnalytics = () => {
+
+  return (
+    <>
+      <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-122356652-1"></script>
+      <script>{`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)}
+      gtag('js', new Date());
+      gtag('config', 'UA-122356652-1');`}
+      </script>
+    </>
+  )
+}
+
 const Header = ({ props }) => {
+  
   return !!props ? (
     <Helmet>
-      <title>{ props.seo.title || 'Huoa.org' }</title>
-      <meta name="description" content={ props.seo.description || 'Welcome to Huoa.org'} />
+      <title>{props.seo.title || 'Huoa.org'}</title>
+      <meta name="description" content={props.seo.description || 'Welcome to Huoa.org'} />
 
-      <meta property="og:title" content={ props.seo.title || 'Huoa.org' } />
-      <meta property="og:description" content={ props.seo.description || 'Welcome to Huoa.org' } />
-      <meta property="og:image" content={ props.seo.image } />
+      <meta property="og:title" content={props.seo.title || 'Huoa.org'} />
+      <meta property="og:description" content={props.seo.description || 'Welcome to Huoa.org'} />
+      <meta property="og:image" content={props.seo.image} />
 
-      <meta name="twitter:title" content={ props.seo.title || 'Huoa.org' } />
-      <meta name="twitter:description" content={ props.seo.description || 'Welcome to Huoa.org' } />
-      <meta name="twitter:image" content={ props.seo.image } />
+      <meta name="twitter:title" content={props.seo.title || 'Huoa.org'} />
+      <meta name="twitter:description" content={props.seo.description || 'Welcome to Huoa.org'} />
+      <meta name="twitter:image" content={props.seo.image} />
       <meta name="twitter:card" content="summary_large_image" />
+
+      {process.env.NODE_ENV === 'production' ? <GoogleAnalytics /> : ''}
+
     </Helmet>
-  ): null;
+  ) : null;
 }
 
 export default Header;
