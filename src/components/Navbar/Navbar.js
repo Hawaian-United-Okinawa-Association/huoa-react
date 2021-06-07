d1a39a322ba46b8888a85af4c35b2c4499aaeef7
david-mcgrouther <dbmcgrouther@gmail.com>
2021-06-07 11:10:18 -0700

    this commit checks if the slug is for an online store on a different site and links accordingly.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -28,10 +28,13 @@ const Navbar = () => {
 
   const renderNavItems = (items) => {
     return items.map((item) => {
+      // console.log(`item.slug ${item.slug}`)
       return !!item && item.slug !== 'home' && (
         <NavItem
           name={item.title}
-          linkTo={ item.slug !== 'shop' ? item.slug :'/' + item.slug }
+          // linkTo={ item.slug !== 'shop' ? item.slug :'/' + item.slug }
+          // linkTo={ '/' + item.slug }
+          linkTo={ item.slug !== 'https://shophuoa.com/' ? '/' + item.slug : 'https://shophuoa.com/'}
           key={item.slug}
           navState={navState}
           setNavState={setNavState}
