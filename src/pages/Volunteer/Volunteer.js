cc895c53f2ebf0b1684dab4b20ddb737474e710b
miraabela <mirabela@hawaii.edu>
2021-10-19 11:41:08 -1000

    add bottom content of volunteers page

--- src/pages/Volunteer/Volunteer.js
+++ src/pages/Volunteer/Volunteer.js
@@ -8,7 +8,7 @@ const Volunteer = ({ data }) => {
 
   if (!data) return null;
   else {
-    const { title, body, opportunities_list, application_form } = data.volunteer_page;
+    const { title, body, opportunities_list, application_form_link, become_body, mail_to_body, fax_to_body, bottom } = data.volunteer_page;
     return (
       <Layout>
         <div className="volunteer__container">
@@ -23,6 +23,37 @@ const Volunteer = ({ data }) => {
           <div className="volunteer__body">
             <Accordion panels={opportunities_list}></Accordion>
           </div>
+          {/* <div className="contact__section">
+          <div className="contact__col">
+            <h4>Become a Volunteer</h4>
+            <div dangerouslySetInnerHTML={{ __html: become_body }} />
+            <Button 
+            link='/volunteer'
+            type='filled'>
+            Application Form
+          </Button>
+        <div className="volunteer__description" dangerouslySetInnerHTML={{__html: bottom}} />
+          </div>
+          <div className="contact__col--right">
+          <h4>Mail to:</h4>
+          <div className="volunteer__description" dangerouslySetInnerHTML={{__html: mail_to_body}} />
+          <h4>Fax to:</h4>
+          <div className="volunteer__description" dangerouslySetInnerHTML={{__html: fax_to_body}} />
+          </div>
+        </div> */}
+
+          <h4>Become a Volunteer</h4>
+          <div className="volunteer__description" dangerouslySetInnerHTML={{__html: become_body}} />
+          <h4>Mail to:</h4>
+          <div className="volunteer__description" dangerouslySetInnerHTML={{__html: mail_to_body}} />
+          <h4>Fax to:</h4>
+          <div className="volunteer__description" dangerouslySetInnerHTML={{__html: mail_to_body}} />
+          <Button 
+            link='/volunteer'
+            type='filled'>
+            Application Form
+          </Button>
+          <div className="volunteer__description" dangerouslySetInnerHTML={{__html: bottom}} />
         </div>
       </Layout>
     );
