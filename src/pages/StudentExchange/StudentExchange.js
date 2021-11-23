deb2225aeb85a5eb1ba5929f3516e5f96ff712e2
Gordon Lewis <glewis@berkeley.edu>
2021-11-22 20:50:02 -0800

    Sets up builder for Student Exchange page

--- src/pages/StudentExchange/StudentExchange.js
+++ src/pages/StudentExchange/StudentExchange.js
@@ -4,13 +4,18 @@ import Layout from 'components/Layout/Layout';
 const StudentExchange = ({ data }) => {
   if (!data) return null;
   else {
-    const { title, body } = data.student_exchange_page;
+    // console.log(data.builder)
+    let { builder } = data;
     return (
       <Layout>
-        <div className="studentexchange__container">
-          <h2 position="left" className="studentexchange__title">{title}</h2>
-          <div className="studentexchange__description" dangerouslySetInnerHTML={{__html: body}} />
-        </div>
+        { builder.map((el, i) =>
+            <div className="studentexchange__container" key={i}>
+              <h2 position="left" className="studentexchange__title">
+                {el.title_body.title}
+              </h2>
+              <div className="studentexchange__description" dangerouslySetInnerHTML = {{ __html: el.title_body.body }} />
+            </div>
+          )}
       </Layout>
     );
   }
