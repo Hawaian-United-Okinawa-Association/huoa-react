cdd14d0b9d8a69485adfa1b0f37734dc90591f9c
Matt Laguardia <matthewlaguardia@gmail.com>
2021-11-30 14:56:07 -0800

    Update routes.js

--- src/config/routes.js
+++ src/config/routes.js
@@ -85,7 +85,7 @@ const Routes = () => {
       </Route>
       <Route path="/about/jen">
         <Header data={ pageState["jen"] } />
-        <Jen data={ pageState["jen"] } />
+        <Jen } />
       </Route>
     </Switch>
   );
