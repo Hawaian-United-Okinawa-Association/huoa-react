7b3ef12cf42b8ce4b1acde7cf16640abcfec54ac
Gordon Lewis <glewis@berkeley.edu>
2022-04-12 12:41:36 -0700

    Removes comments

--- src/pages/EventsCalendar/EventsCalendar.js
+++ src/pages/EventsCalendar/EventsCalendar.js
@@ -15,15 +15,6 @@ const EventsCalendar = ({ data }) => {
     <Layout>
       <Container>
         <h1 className="events-calendar__title">Events Calendar</h1>
-        {/* <iframe
-          src="https://www.google.com/calendar/embed?showTitle=0&showPrint=0&height=500&wkst=1&bgcolor=%23FFFFFF&src=huoacalendar%40gmail.com&color=%232952A3&src=j7d5p9evkshjmhik3v9po98kus%40group.calendar.google.com&color=%23B1440E&ctz=Pacific%2FHonolulu"
-          width="100%"
-          height="500"
-          frameBorder="0"
-          scrolling="no"
-          title="Events Calendar"
-          className="events-calendar__calendar"
-        ></iframe> */}
         <div className="responsiveCal">
           <div className="deskContent">
             <iframe
