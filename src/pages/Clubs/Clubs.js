057cbfdc2275143cf0b7b943efc272a08c6eff33
FirdaousseErrazi <firdaousse1995@gmail.com>
2022-05-12 15:17:20 +0000

    update(Clubs) : adds search filters and styling

--- src/pages/Clubs/Clubs.js
+++ src/pages/Clubs/Clubs.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Layout from "components/Layout/Layout";
 
+import { ReactComponent as Chevron } from 'assets/chevron-down.svg';
+
 const Clubs = ({ data }) => {
   let clubs = useSelector((state) => state.clubs);
+  const [first, setfirst] = useState('');
+  const filterClubs = club => first.split(' ').every(word => club.title.rendered.toLowerCase().includes(word));
+  const resultsNumber = clubs.filter(filterClubs).length;
 
   if (!data) return null;
   else {
     const { title, body } = data.join_a_club_page;
-    const clubsList = clubs.map((club) => {
+    const clubsList = clubs.filter(filterClubs).map((club) => {
       return (
         <li key={club.id}>
           <Link className="clubs__item-link" to={"/clubs/" + club.slug}>
-            {club.title.rendered}
+            {club.title.rendered} <Chevron />
           </Link>
         </li>
       );
@@ -24,10 +29,12 @@ const Clubs = ({ data }) => {
           <h2 position="left" className="clubs__title">{title}</h2>
           <div className="clubs__description" dangerouslySetInnerHTML={{__html: body}} />
           <div className="clubs__body">
+            <label htmlFor="search" className="clubs__label">Our {clubs.length} Clubs</label>
+            <input id="search" onKeyUp={ e => setfirst(e.target.value.trim())} className="clubs__search" placeholder="Find a club"/></div>
+            {first && <p>{resultsNumber} result{resultsNumber > 1 ? 's' : ''} found</p>}
             {!!clubs && (
               <ul className="clubs__items">{clubsList}</ul>
             )}
-          </div>
         </div>
       </Layout>
     );
