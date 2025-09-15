058e464128ae529ac3d8d70d5e506e1b9335bac9
Abdessamad Nouri <nouri.abde.dev@gmail.com>
2025-09-15 10:26:06 +0000

    chore: add Donor Snap Form to Donate page

--- src/pages/Donate/Donate.js
+++ src/pages/Donate/Donate.js
@@ -13,6 +13,7 @@ import React from 'react';
 import Layout from 'components/Layout/Layout';
 import DonateSectionOne from 'sections/DonateSectionOne/DonateSectionOne';
 import DonateSectionTwo from 'sections/DonateSectionTwo/DonateSectionTwo';
+import DonorSnapForm from 'sections/DonateSectionThree/DonorSnapForm';
 
 const Donate = ({ data }) => {
   if (!data) return null;
@@ -22,6 +23,7 @@ const Donate = ({ data }) => {
       <Layout>
         <DonateSectionOne data={data.donate_section_one} />
         <DonateSectionTwo data={data.donate_section_two} />
+        <DonorSnapForm/>
       </Layout>
     </div>
     )
