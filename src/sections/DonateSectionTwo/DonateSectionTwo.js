ecd953a773e42ae1c14ff55416ea8daf336f94b2
Abdessamad Nouri <nouri.abde.dev@gmail.com>
2025-09-19 11:02:36 +0000

    chore: update Donate Section Two PayPal icon

--- src/sections/DonateSectionTwo/DonateSectionTwo.js
+++ src/sections/DonateSectionTwo/DonateSectionTwo.js
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import { ReactComponent as Donate } from 'assets/donate.svg';
-import { ReactComponent as Amazon } from 'assets/amazon.svg';
-import { ReactComponent as Car } from 'assets/car-2.svg';
+import { ReactComponent as Donate } from "assets/donate.svg";
+import { ReactComponent as Paypal } from "assets/paypal.svg";
+import { ReactComponent as Car } from "assets/car-2.svg";
 
-
-const DonateSectionOne = ({ data }) => {
-  const columnArray = Object.keys(data.body).map(i => data.body[i]);
+const DonateSectionTwo = ({ data }) => {
+  const columnArray = Object.keys(data.body).map((i) => data.body[i]);
   const cols = columnArray.map((col, i) => {
     const icons = {
-      'tree-planting': <Donate className='donate-section-two__icon'/>,
-      'coin-in-hand': <Amazon className='donate-section-two__icon'/>,
-      'car': <Car className='donate-section-two__icon'/>
-    }
+      "tree-planting": <Donate className="donate-section-two__icon" />,
+      "coin-in-hand": <Paypal className="donate-section-two__icon" />,
+      car: <Car className="donate-section-two__icon" />,
+    };
 
     return (
-      <div className='donate-section-two__col' key={i}>
-        <div className='donate-section-two__icon-bg'>{icons[col.icon]}</div>
-        <h4 className='donate-section-two__subtitle'>{col.subtitle}</h4>
-        <div className='donate-section-two__text' dangerouslySetInnerHTML={{__html: col.text}}/>
+      <div className="donate-section-two__col" key={i}>
+        <div className="donate-section-two__icon-bg">{icons[col.icon]}</div>
+        <h4 className="donate-section-two__subtitle">{col.subtitle}</h4>
+        <div
+          className="donate-section-two__text"
+          dangerouslySetInnerHTML={{ __html: col.text }}
+        />
       </div>
-    )
-  })
+    );
+  });
 
   return (
-    <div className='donate-section-two'>
-      <h2 className='donate-section-two__title'>{data.title}</h2>
-      <div className='donate-section-two__body'>
-        {cols}
-      </div>
+    <div className="donate-section-two">
+      <h2 className="donate-section-two__title">{data.title}</h2>
+      <div className="donate-section-two__body">{cols}</div>
     </div>
-  )
-}
+  );
+};
 
-export default DonateSectionOne;
+export default DonateSectionTwo;
